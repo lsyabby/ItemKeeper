fbef12cee4336946eddeacf2b831f467db955947
lsyabby <amyandlisa21022@gmail.com>
2018-05-01 11:53:50 +0800

    added ItemListCollectionViewCell xib

---
+++ project_2/View/ItemListCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemListCollectionViewCell.swift
+//  project_2
+//
+//  Created by 李思瑩 on 2018/5/1.
+//  Copyright © 2018年 李思瑩. All rights reserved.
+//
+
+import UIKit
+
+class ItemListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
