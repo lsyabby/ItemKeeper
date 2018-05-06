84d1f05d037c2c6989a1e26bac6536bcda687dee
lsyabby <amyandlisa21022@gmail.com>
2018-05-06 18:58:07 +0800

    fixed instocklistviewcontroller

---
+++ project_2/Controller/InStock/InStock5ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  InStock5ViewController.swift
+//  project_2
+//
+//  Created by 李思瑩 on 2018/5/6.
+//  Copyright © 2018年 李思瑩. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseCore
+import SDWebImage
+import ZHDropDownMenu
+
+class InStock5ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ZHDropDownMenuDelegate {
+
+    @IBOutlet weak var filterDropDownMenu: ZHDropDownMenu!
+    @IBOutlet weak var instock5TableView: UITableView!
+    var ref: DatabaseReference!
+    var items: [ItemList] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        filterDropDownMenu.options = ["餅乾", "止痛藥", "乳液"]
+        filterDropDownMenu.editable = true //可编辑
+        filterDropDownMenu.delegate = self
+        
+        instock5TableView.delegate = self
+        instock5TableView.dataSource = self
+        
+        let nib = UINib(nibName: "ItemListTableViewCell", bundle: nil)
+        instock5TableView.register(nib, forCellReuseIdentifier: "ItemListTableCell")
+        
+        ref = Database.database().reference()
+        self.ref.child("items/mxI0h7c9GlR1eVZRqH8Sfs1LP6B2").observeSingleEvent(of: .value) { (snapshot) in
+            //        self.ref.child("items/\(Auth.auth().currentUser?.uid)").observeSingleEvent(of: .value) { (snapshot) in
+            guard let value = snapshot.value as? [String: Any] else { return }
+            var allItems = [ItemList]()
+            for item in value {
+                print("===== item =====")
+                print(item)
+                if let list = item.value as? [String: Any] {
+                    print("===== list =====")
+                    print(list)
+                    let name = list["name"] as? String
+                    let itemId = list["id"] as? Int
+                    let image = list["imageURL"] as? String
+                    let createdate = list["createdate"] as? String
+                    let enddate = list["enddate"] as? String
+                    let alertdate = list["alertdate"] as? String
+                    let category = list["category"] as? String
+                    let instock = list["instock"] as? Int
+                    let isInstock = list["isInstock"] as? Bool
+                    let otehrs = list["others"] as? String ?? ""
+                    let remainday = list["remainday"] as? Int
+                    let info = ItemList(name: name!, itemId: itemId!, imageURL: image!, createdate: createdate!, enddate: enddate!, alertdate: alertdate!, category: category!, instock: instock!, isInstock: isInstock!, others: otehrs, remainday: remainday!)
+                    if info.isInstock == true {
+                        allItems.append(info)
+                    }
+                }
+            }
+            self.items = allItems
+            self.instock5TableView.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "ItemListTableCell", for: indexPath) as? ItemListTableViewCell {
+            cell.itemInstockImageView.isHidden = true
+            cell.itemNameLabel.text = items[indexPath.row].name
+            cell.itemIdLabel.text = String(describing: items[indexPath.row].itemId)
+            cell.itemImageView.sd_setImage(with: URL(string: items[indexPath.row].imageURL))
+            cell.itemEnddateLabel.text = items[indexPath.row].enddate
+            cell.itemCategoryLabel.text = "# \(items[indexPath.row].category)"
+            cell.itemRemaindayLabel.text = "還剩 \(items[indexPath.row].remainday) 天"
+            cell.itemInstockLabel.text = "x \(items[indexPath.row].instock)"
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func dropDownMenu(_ menu: ZHDropDownMenu, didEdit text: String) {
+        filterDropDownMenu.options.append(text) //編輯後加入list
+        print("\(menu) input text \(text)")
+    }
+    
+    func dropDownMenu(_ menu: ZHDropDownMenu, didSelect index: Int) {
+        print("\(menu) choosed at index \(index)")
+    }
+
+}
--- project_2/Controller/InStock/InStockListViewController.swift
+++ project_2/Controller/InStock/InStockListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ItemListViewController.swift
+//  InStockViewController.swift
 //  project_2
 //
 //  Created by 李思瑩 on 2018/5/1.
@@ -9,35 +9,34 @@
 import UIKit
 
 
-class ItemListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
-
-    @IBOutlet weak var itemCategoryCollectionView: UICollectionView!
-    @IBOutlet weak var itemListScrollView: UIScrollView!
-    let list: [String] = ["總攬", "食品", "藥品", "美妝", "日用品", "其他"]
+class InStockListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
+    
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    @IBOutlet weak var listScrollView: UIScrollView!
+    let list: [String] = ["食品", "藥品", "美妝", "日用品", "其他"]
     var selectedBooling: [Bool] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.tintColor = UIColor.lightGray
         
-        itemCategoryCollectionView.showsHorizontalScrollIndicator = false
-        itemListScrollView.showsHorizontalScrollIndicator = false
+        categoryCollectionView.showsHorizontalScrollIndicator = false
+        listScrollView.showsHorizontalScrollIndicator = false
         
-        itemCategoryCollectionView.delegate = self
-        itemCategoryCollectionView.dataSource = self
-        itemListScrollView.delegate = self
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
+        listScrollView.delegate = self
         
         let upnib = UINib(nibName: "CategoryCollectionViewCell", bundle: nil)
-        itemCategoryCollectionView.register(upnib, forCellWithReuseIdentifier: "CategoryCollectionCell")
+        categoryCollectionView.register(upnib, forCellWithReuseIdentifier: "CategoryCollectionCell")
         
         // vc
-        let storyboard = UIStoryboard(name: "ItemList", bundle: nil)
-        guard let item0vc = storyboard.instantiateViewController(withIdentifier: "Item0") as? Item0ViewController,
-            let item1vc = storyboard.instantiateViewController(withIdentifier: "Item1") as? Item1ViewController,
-            let item2vc = storyboard.instantiateViewController(withIdentifier: "Item2") as? Item2ViewController,
-            let item3vc = storyboard.instantiateViewController(withIdentifier: "Item3") as? Item3ViewController,
-            let item4vc = storyboard.instantiateViewController(withIdentifier: "Item4") as? Item4ViewController,
-            let item5vc = storyboard.instantiateViewController(withIdentifier: "Item5") as? Item5ViewController else { return }
+        let storyboard = UIStoryboard(name: "InStock", bundle: nil)
+        guard let item1vc = storyboard.instantiateViewController(withIdentifier: "InStock1") as? InStock1ViewController,
+            let item2vc = storyboard.instantiateViewController(withIdentifier: "InStock2") as? InStock2ViewController,
+            let item3vc = storyboard.instantiateViewController(withIdentifier: "InStock3") as? InStock3ViewController,
+            let item4vc = storyboard.instantiateViewController(withIdentifier: "InStock4") as? InStock4ViewController,
+            let item5vc = storyboard.instantiateViewController(withIdentifier: "InStock5") as? InStock5ViewController else { return }
         
         let bounds = UIScreen.main.bounds
         // ???
@@ -45,14 +44,14 @@ class ItemListViewController: UIViewController, UICollectionViewDelegate, UIColl
         print(width)
         print(self.view.bounds.width)
         let height = bounds.size.height
-        itemListScrollView.contentSize = CGSize(width: CGFloat(list.count) * width, height: 0)
-        let vcArray = [item0vc, item1vc, item2vc, item3vc, item4vc, item5vc]
+        listScrollView.contentSize = CGSize(width: CGFloat(list.count) * width, height: 0)
+        let vcArray = [item1vc, item2vc, item3vc, item4vc, item5vc]
         var idx: Int = 0
         for itemVC in vcArray {
             addChildViewController(itemVC)
             let originX: CGFloat = CGFloat(idx) * width
             itemVC.view.frame = CGRect(x: originX, y: 0, width: 375, height: height)
-            itemListScrollView.addSubview(itemVC.view)
+            listScrollView.addSubview(itemVC.view)
             itemVC.didMove(toParentViewController: self)
             idx += 1
         }
@@ -91,49 +90,46 @@ class ItemListViewController: UIViewController, UICollectionViewDelegate, UIColl
             selectedBooling[iii] = false
         }
         selectedBooling[indexPath.item] = true
-        itemCategoryCollectionView.reloadData()
+        categoryCollectionView.reloadData()
         
         let itemNum = indexPath.item
-        itemListScrollView.setContentOffset(CGPoint(x: view.frame.width * CGFloat(itemNum), y: 0), animated: true)
+        listScrollView.setContentOffset(CGPoint(x: view.frame.width * CGFloat(itemNum), y: 0), animated: true)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard let categoryCollectionViewFlowLayout = itemCategoryCollectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        guard let categoryCollectionViewFlowLayout = categoryCollectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
         let categoryDistanceBetweenItemsCenter = categoryCollectionViewFlowLayout.minimumLineSpacing + categoryCollectionViewFlowLayout.itemSize.width
         let scrollViewDistanceBetweenItemsCenter = UIScreen.main.bounds.width
         let offsetFactor = categoryDistanceBetweenItemsCenter / scrollViewDistanceBetweenItemsCenter
         
-        if (scrollView == itemCategoryCollectionView) {
+        if (scrollView == categoryCollectionView) {
             let xOffset = scrollView.contentOffset.x - scrollView.frame.origin.x
-            itemListScrollView.contentOffset.x = xOffset / offsetFactor
-        } else if (scrollView == itemListScrollView) {
+            listScrollView.contentOffset.x = xOffset / offsetFactor
+        } else if (scrollView == listScrollView) {
             let xOffset = scrollView.contentOffset.x - scrollView.frame.origin.x
-            itemCategoryCollectionView.contentOffset.x = xOffset * offsetFactor
+            categoryCollectionView.contentOffset.x = xOffset * offsetFactor
         }
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageNum = Int(round(itemListScrollView.contentOffset.x / itemListScrollView.frame.size.width))
+        let pageNum = Int(round(listScrollView.contentOffset.x / listScrollView.frame.size.width))
         
         switch pageNum {
         case 0 :
-            collectionView(itemCategoryCollectionView, didSelectItemAt: [0, 0])
-            itemCategoryCollectionView.scrollToItem(at: [0, 0], at: .centeredHorizontally, animated: true)
+            collectionView(categoryCollectionView, didSelectItemAt: [0, 0])
+            categoryCollectionView.scrollToItem(at: [0, 0], at: .centeredHorizontally, animated: true)
         case 1 :
-            collectionView(itemCategoryCollectionView, didSelectItemAt: [0, 1])
-            itemCategoryCollectionView.scrollToItem(at: [0, 1], at: .centeredHorizontally, animated: true)
+            collectionView(categoryCollectionView, didSelectItemAt: [0, 1])
+            categoryCollectionView.scrollToItem(at: [0, 1], at: .centeredHorizontally, animated: true)
         case 2 :
-            collectionView(itemCategoryCollectionView, didSelectItemAt: [0, 2])
-            itemCategoryCollectionView.scrollToItem(at: [0, 2], at: .centeredHorizontally, animated: true)
+            collectionView(categoryCollectionView, didSelectItemAt: [0, 2])
+            categoryCollectionView.scrollToItem(at: [0, 2], at: .centeredHorizontally, animated: true)
         case 3 :
-            collectionView(itemCategoryCollectionView, didSelectItemAt: [0, 3])
-            itemCategoryCollectionView.scrollToItem(at: [0, 3], at: .centeredHorizontally, animated: true)
+            collectionView(categoryCollectionView, didSelectItemAt: [0, 3])
+            categoryCollectionView.scrollToItem(at: [0, 3], at: .centeredHorizontally, animated: true)
         case 4 :
-            collectionView(itemCategoryCollectionView, didSelectItemAt: [0, 4])
-            itemCategoryCollectionView.scrollToItem(at: [0, 4], at: .centeredHorizontally, animated: true)
-        case 5 :
-            collectionView(itemCategoryCollectionView, didSelectItemAt: [0, 5])
-            itemCategoryCollectionView.scrollToItem(at: [0, 5], at: .centeredHorizontally, animated: true)
+            collectionView(categoryCollectionView, didSelectItemAt: [0, 4])
+            categoryCollectionView.scrollToItem(at: [0, 4], at: .centeredHorizontally, animated: true)
         default:
             print("unknow location")
             return
@@ -142,7 +138,7 @@ class ItemListViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     func setupListGridView() {
         let screenSize = UIScreen.main.bounds
-        if let categoryCollectionViewFlowLayout = itemCategoryCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+        if let categoryCollectionViewFlowLayout = categoryCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             categoryCollectionViewFlowLayout.itemSize = CGSize(width: screenSize.width / 2, height: 50)
             categoryCollectionViewFlowLayout.minimumInteritemSpacing = 0
             categoryCollectionViewFlowLayout.minimumLineSpacing = 10
@@ -150,5 +146,5 @@ class ItemListViewController: UIViewController, UICollectionViewDelegate, UIColl
             categoryCollectionViewFlowLayout.sectionInset = UIEdgeInsetsMake(0, categoryCollectionViewSectionInset, 0, categoryCollectionViewSectionInset)
         }
     }
-
+    
 }
