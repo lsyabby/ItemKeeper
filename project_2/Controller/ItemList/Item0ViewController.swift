df9bc0b26feb7c9c41e698447a4d68bb42e1fa25
lsyabby <amyandlisa21022@gmail.com>
2018-05-06 15:28:36 +0800

    added item0 - item5 viewcontroller

---
+++ project_2/Controller/ItemList/Item0ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Item0ViewController.swift
+//  project_2
+//
+//  Created by 李思瑩 on 2018/5/6.
+//  Copyright © 2018年 李思瑩. All rights reserved.
+//
+
+import UIKit
+
+class Item0ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
