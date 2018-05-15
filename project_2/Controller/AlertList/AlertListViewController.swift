da83b8ced8ba6cb2e142eab7b628c6206d08f121
lsyabby <amyandlisa21022@gmail.com>
2018-05-15 13:04:45 +0800

    start to add alertList

--- project_2/Controller/AlertList/AlertListViewController.swift
+++ project_2/Controller/AlertList/AlertListViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class AlertListViewController: UIViewController {
+class AlertListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +23,18 @@ class AlertListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+}
+
+
+extension AlertListViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
